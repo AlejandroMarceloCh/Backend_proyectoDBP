1873f496263323494aa247c7d8729108e5972297
AlejandroMarceloCh <alejandro.marcelo@utec.edu.pe>
2023-05-15 14:54:34 -0500

    first commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import User
+from app.models import User, Client, Product, Inventory, Purchase, Sale
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user
 
@@ -15,19 +15,26 @@ def users():
         users = User.query.all()
         )
 
+@app.route("/sale")
+def sale():
+    return render_template("sale.html")
+
+@app.route("/purchase")
+def purchase():
+    return render_template("purchase.html")
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    
+
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("purchase"))
 
     if request.method == "POST":
         try:
             name = request.form.get("name")
             email = request.form.get("email")
             password = request.form.get("password")
-
         
             with app.app_context():
                 u = User(name, email, password)
@@ -35,16 +42,39 @@ def signup():
                 db.session.commit()
 
         except:
-            flash("Error creating user", "danger")
+            flash("Error creating Employee", "danger")
 
     return render_template("signup.html", tittle = "Signup")
+
+
+@app.route("/signupclient", methods=["GET", "POST"])
+def signupclient():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for("sale"))
+
+    if request.method == "POST":
+        try:
+            name = request.form.get("name")
+            email = request.form.get("email")
+            password = request.form.get("password")
+
+            with app.app_context():
+                u = Client(name, email, password)
+                db.session.add(u)
+                db.session.commit()
+
+        except:
+            flash("Error creating Client", "danger")
+
+    return render_template("signupclient.html", tittle = "SignupClient")
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("purchase"))
 
     if request.method == "POST":
         with app.app_context():
@@ -55,7 +85,7 @@ def login():
             if user:    
                 if user.check_password(password):
                     login_user(user)
-                    return redirect(url_for("index")) #este "index" hace referencia a la funcion "index" de la linea 7
+                    return redirect(url_for("purchase"))
                 else:
                     flash("Wrong password", "danger")
 
@@ -64,10 +94,45 @@ def login():
             
     return render_template("login.html", tittle = "Login")
 
+
+
+@app.route('/loginclient', methods=['GET', 'POST'])
+def loginclient():
+    #Cambiar la redireccion a la interfaz que muestra los inventarios en lugar de index
+    if current_user.is_authenticated:
+        return redirect(url_for("sale"))
+
+
+    if request.method == "POST":
+        with app.app_context():
+            email = request.form.get("email")
+            password = request.form.get("password")
+            user = Client.query.filter_by(email=email).first()
+
+            if user: 
+                if user.check_password(password):
+                    login_user(user)
+                    return redirect(url_for("sale")) #este "index" hace referencia a la funcion "index" de la linea 7
+                else:
+                    flash("Wrong password", "danger")
+
+            else:
+                flash("User with this email does not exist", "danger")
+            
+    return render_template("loginclient.html", tittle = "LoginClient")
+
+
+@app.route("/makepurchase")
+def makepurchase():
+    #falta agregar la logica para meter las compras
+    return render_template("makepurchase.html")
+
+
 @app.route("/logout")
 def logout():
     if not current_user.is_authenticated:
         return redirect(url_for("login"))
     logout_user()
     return redirect(url_for("index"))
+
 
